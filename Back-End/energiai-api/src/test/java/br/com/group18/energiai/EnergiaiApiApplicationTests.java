6def92aef73dc9941a62e0f80eb83781094b0292
Eduardo Gonçalves <eduardouc388@gmail.com>
2026-07-13 22:18:55 -0300

    Back-End Init

---
+++ Back-End/energiai-api/src/test/java/br/com/group18/energiai/EnergiaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.group18.energiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergiaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
