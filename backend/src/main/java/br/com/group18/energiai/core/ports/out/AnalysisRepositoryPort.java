231c10aa39e4e345fb229f332f911727cd25af58
DessimA <j.anderson.mect@gmail.com>
2026-07-16 23:14:02 -0300

    feat: add English-named ports (GenerateAnalysisUseCase, AnalysisRepositoryPort, UserRepositoryPort)

---
+++ backend/src/main/java/br/com/group18/energiai/core/ports/out/AnalysisRepositoryPort.java
@@ -0,0 +1,10 @@
+package br.com.group18.energiai.core.ports.out;
+
+import br.com.group18.energiai.core.domain.model.EnergyAnalysis;
+import java.util.List;
+
+public interface AnalysisRepositoryPort {
+  EnergyAnalysis save(EnergyAnalysis analysis);
+
+  List<EnergyAnalysis> listAll();
+}
---
+++ backend/src/main/java/br/com/group18/energiai/core/ports/out/UserRepositoryPort.java
@@ -0,0 +1,12 @@
+package br.com.group18.energiai.core.ports.out;
+
+import br.com.group18.energiai.core.domain.model.User;
+import java.util.Optional;
+
+public interface UserRepositoryPort {
+  User save(User user);
+
+  Optional<User> findByEmail(String email);
+
+  Optional<User> findById(Long id);
+}
