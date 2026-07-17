8ace3cfb98a519b5d53105ca21f58a9419bda539
DessimA <j.anderson.mect@gmail.com>
2026-07-16 23:14:25 -0300

    refactor: remove Portuguese-named ports (GerarAnaliseUseCase, AnaliseRepositoryPort, UsuarioRepositoryPort)

--- backend/src/main/java/br/com/group18/energiai/core/ports/out/AnaliseRepositoryPort.java
+++
@@ -1,9 +0,0 @@
-package br.com.group18.energiai.core.ports.out;
-
-import br.com.group18.energiai.core.domain.model.AnaliseEnergia;
-import java.util.List;
-
-public interface AnaliseRepositoryPort {
-    AnaliseEnergia salvar(AnaliseEnergia analise);
-    List<AnaliseEnergia> listarTodas();
-}
